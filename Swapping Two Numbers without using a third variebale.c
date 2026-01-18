b7ff45ee189b54654110e6b5cc90c404520ae8de
samadritakundu07-boop <samadritakundu07@gmail.com>
2026-01-18 21:00:33 +0530

    Create Swapping Two Numbers without using a third variebale.c
    update

---
+++ Swapping Two Numbers without using a third variebale.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ int a=5;
+ int b=6;
+ printf("before swapping:a=%d,b=%d\n",a,b);
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ printf("after swapping:a=%d,b=%d\n",a,b);
+
+    return 0;
+}
